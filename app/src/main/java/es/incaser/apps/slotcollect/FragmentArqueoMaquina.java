db5ec111b7efa9f06ba529029524886c8e5e8e65
sergio-incaser <sergio@incaser.es>
2014-09-28 12:38:22 +0200

    [ADD] Add fragment activities to slidepage navigation

---
+++ app/src/main/java/es/incaser/apps/slotcollect/FragmentArqueoMaquina.java
@@ -0,0 +1,30 @@
+package es.incaser.apps.slotcollect;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sergio on 28/09/14.
+ */
+public class FragmentArqueoMaquina extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        // Inflamos la Vista que se debe mostrar en pantalla.
+        View rootView = inflater.inflate(R.layout.fragment_slide_page_contadores, container,
+                false);
+        // Creamos instancia del TextView.
+        //TextView tvText =  (TextView)rootView.findViewById(R.id.tvText);
+        // Recogemos el texto que guardamos al crear el Fragment.
+        //String text = getArguments().getString(KEY_REG_TEXT);
+        // Mostramos el texto en el TextView.
+        //tvText.setText(text);
+
+        // Devolvemos la vista para que se muestre en pantalla.
+        return rootView;
+    }
+}
---
+++ app/src/main/java/es/incaser/apps/slotcollect/ScreenSlidePagerRecaudacion.java
@@ -0,0 +1,55 @@
+package es.incaser.apps.slotcollect;
+
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by sergio on 28/09/14.
+ */
+public class ScreenSlidePagerRecaudacion extends FragmentActivity implements ActionBar.TabListener{
+    /**
+     * The number of pages (wizard steps) to show in this demo.
+     */
+    private static final int NUM_PAGES = 3;
+
+    /**
+     * The pager widget, which handles animation and allows swiping horizontally to access previous
+     * and next wizard steps.
+     */
+    private ViewPager mPager;
+
+    /**
+     * The pager adapter, which provides the pages to the view pager widget.
+     */
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slide_screen_recaudacion);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+}
